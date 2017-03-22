752ddca794edaf88d721eef64d3d780e977fc96c
Morgan Fogarty <mofo37@gmail.com>
2017-03-22 14:59:09 -0700

    added token

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ var express = require('express'),
 function proxyGitHub(request, response) {
   console.log('Routing Github request for', request.params[0]);
   (requestProxy({
-    url: 'https://api.github.com/' + request.params[0]
+    url: 'https://api.github.com/' + request.params[0],
+    header: process.env.GITHUBTOKEN
   }))(request, response);
 };
 
